6e65987e422c48d6ae45a8d105a7583ccfbdab80
= <=>
2019-09-05 16:22:57 +0200

    Test

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
 // Modules
 const express = require("express");
+const BodyParser = require("body-parser");
+// Initalizes
+const app = express();
+app.use(BodyParser.urlencoded({
+    extended: false
+}));
+// The Routes
+//app.use("/rest/search")
+// Listens
+app.listen(80);
