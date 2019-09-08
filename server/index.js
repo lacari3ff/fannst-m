a508d3daa4ca5eaf86112e0c978fb88094178a7d
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-09-08 20:50:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,39 @@
 // The modules
-const express       = require("express");
-const cors          = require("cors");
-const BodyParser    = require("body-parser");
-const path          = require("path");
+const express = require("express");
+const cors = require("cors");
+const BodyParser = require("body-parser");
+const path = require("path");
 // Initializes
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
-app.use(BodyParser.urlencoded({
+app.use(
+  BodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 // The Routes
-const SearchRoute       = require("./routes/search-route");
-const WeatherRoute      = require("./routes/weather-route");
-const NewsRoute         = require("./routes/news-route");
+const SearchRoute = require("./routes/search-route");
+const WeatherRoute = require("./routes/weather-route");
+const NewsRoute = require("./routes/news-route");
 app.use("/rest/search", SearchRoute);
 app.use("/rest/weather", WeatherRoute);
 app.use("/rest/news", NewsRoute);
-app.get("*", function (req, res, next) {
-    let domain = req.hostname;
+app.get("*", function(req, res, next) {
+  let domain = req.hostname;
 
-    switch(domain) {
-        case "localhost": {
-            res.sendFile(path.resolve(`${__dirname}/views/news/index.html`));
-            break;
-        }
-        default: {
-            break;
-        }
+  switch (domain) {
+    case "news.fannst.nl": {
+      res.sendFile(path.resolve(`${__dirname}/views/news/index.html`));
+      break;
     }
+    default: {
+      res.json({
+        status: false
+      });
+      break;
+    }
+  }
 });
 // Listens
 app.listen(80);
