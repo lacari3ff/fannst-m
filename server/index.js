8c67c58b47220e1fd70715c86b843bf70c0c4ce9
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-09-08 20:49:15 +0200

    s

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express       = require("express");
 const cors          = require("cors");
 const BodyParser    = require("body-parser");
+const path          = require("path");
 // Initializes
 const app = express();
 app.use(express.static("public"));
@@ -16,5 +17,18 @@ const NewsRoute         = require("./routes/news-route");
 app.use("/rest/search", SearchRoute);
 app.use("/rest/weather", WeatherRoute);
 app.use("/rest/news", NewsRoute);
+app.get("*", function (req, res, next) {
+    let domain = req.hostname;
+
+    switch(domain) {
+        case "localhost": {
+            res.sendFile(path.resolve(`${__dirname}/views/news/index.html`));
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+});
 // Listens
 app.listen(80);
---
+++ server/public/news/precache-manifest.533393a76315cc272f4fd03a604c5bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9852076872af653548492cd628dfcab7",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "bee6527f30fc6ec4514e",
+    "url": "/news/static/css/main.68ae073b.chunk.css"
+  },
+  {
+    "revision": "4fdee46f634340b853a8",
+    "url": "/news/static/js/2.3cc2a6e7.chunk.js"
+  },
+  {
+    "revision": "bee6527f30fc6ec4514e",
+    "url": "/news/static/js/main.9bc262db.chunk.js"
+  },
+  {
+    "revision": "26a1f2c0871f2661e70b",
+    "url": "/news/static/js/runtime~main.cc07ad63.js"
+  },
+  {
+    "revision": "508eefb42001fb88c3a0d4dd9856e860",
+    "url": "/news/static/media/fannst-news.508eefb4.png"
+  }
+]);
