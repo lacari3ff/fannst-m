70bad51c07112792dcac82b292ed2a796ef25307
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-09-08 20:32:25 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
 // The modules
-const mongoose      = require("mongoose");
 const express       = require("express");
 const cors          = require("cors");
 const BodyParser    = require("body-parser");
-// Connects mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", {
-    useNewUrlParser: true,
-    dbName: "fannst_index"
-});
 // Initializes
 const app = express();
 app.use(express.static("public"));
