4b03c1135b69b46a9240c1d91b71b3583a215d24
lacari3ff <42448960+lacari3ff@users.noreply.github.com>
2019-09-13 12:17:36 +0200

    asd

--- server/routes/fmail-route.js
+++ server/routes/fmail-route.js
@@ -3,7 +3,7 @@ const express = require("express");
 // Initializes
 const Router = express.Router();
 // Controllers
-const FmailController = require("../controllers/flmail-controller");
+const FmailController = require("../controllers/fmail-controller");
 // Handles the request
 Router.post("/send", function (req, res, next) {
     FmailController.send(req, res, next);
